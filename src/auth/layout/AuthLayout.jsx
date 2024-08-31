df7be96d941aa3caffeb79d4eed91652ff45e932
yerytech <202000854@p.uapa.edu.do>
2024-08-31 14:27:23 -0400

    register add

---
+++ src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,35 @@
+import { Grid2, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+export const AuthLayout = ({ children, title }) => {
+  return (
+    <Grid2
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid2
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{
+          width: { md: 460 },
+          backgroundColor: "white",
+          padding: 3,
+          borderRadius: 2,
+        }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          {title}
+        </Typography>
+        {children}
+      </Grid2>
+    </Grid2>
+  );
+};
+AuthLayout.propTypes = {
+  children: PropTypes.element,
+  title: PropTypes.string,
+};
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,51 @@
+import { Link as RouterLink } from "react-router-dom";
+
+import { Button, Grid2, Link, TextField } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <AuthLayout title="Register">
+      <form>
+        <Grid2 container>
+          <Grid2 item size={{ xs: 12, md: 20 }} sx={{ mt: 2 }}>
+            <TextField
+              label="User"
+              type="text"
+              placeholder="User Name"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={{ xs: 12, md: 20 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="email@email.com"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 item size={{ xs: 12, md: 20 }} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 container spacing={1} sx={{ mb: 1, mt: 1 }} size={{ xs: 12 }}>
+            <Grid2 item size={{ xs: 12, sm: 12 }}>
+              <Button variant="contained" fullWidth>
+                Registrar
+              </Button>
+            </Grid2>
+          </Grid2>
+        </Grid2>
+        <Grid2 container direction="row" justifyContent="flex-end">
+          <Link component={RouterLink} color="inherit" to="/auth/login">
+            Ya tienes una cuenta?
+          </Link>
+        </Grid2>
+      </form>
+    </AuthLayout>
+  );
+};
